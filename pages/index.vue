1da0371f698dbbbc6b5cfa19c49065b406084039
Allandt Bik-Elliott <allandt@gmail.com>
2021-01-26 23:16:16 +0000

    switch index.vue to 4 spaces

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,47 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        overbyte.co.uk
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="container">
+
+        <nav>
+            <div class="logo">
+                Allandt Bik-Elliott
+            </div>
+            <a href="#home">Home</a>
+            <a href="#work">Work</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+        </nav>
+        <div>
+            <Logo />
+            <h1>
+                Hello!
+            </h1>
+
+            <h2>
+                My name is Allandt (I'll tell you how it's pronounced, if you fancy a
+                chat), a developer living in Hertford, a train-ride away from London.
+            </h2>
+
+            <h3>
+                I have experience creating front-ends for websites, installations for
+                events and mobile apps.
+            </h3>
+
+            <div class="links">
+                <a href="https://nuxtjs.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="button--green">
+                    Documentation
+                </a>
+                <a href="https://github.com/nuxt/nuxt.js"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="button--grey">
+                    GitHub
+                </a>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -33,16 +50,16 @@ export default {}
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
 }
 
 .title {
-  font-family:
+    font-family:
     'Quicksand',
     'Source Sans Pro',
     -apple-system,
@@ -52,22 +69,22 @@ export default {}
     'Helvetica Neue',
     Arial,
     sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
 }
 
 .subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
 }
 
 .links {
-  padding-top: 15px;
+    padding-top: 15px;
 }
 </style>
