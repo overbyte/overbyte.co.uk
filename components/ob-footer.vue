1824d542385e49b5ea753c2d519ac626ff5dba3e
Allandt Bik-Elliott <allandt@gmail.com>
2021-01-27 23:54:30 +0000

    add missing header and footer components

---
+++ components/ob-footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer>
+        Copyright &copy; 2021 Allandt Bik-Elliott
+    </footer>
+</template>
+
+<style lang="scss">
+footer {
+    width: 100%;
+    text-align: center;
+    padding: 3em;
+    font-weight: 400;
+    color: var(--tertiary);
+}
+</style>
---
+++ components/ob-header.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="main-nav">
+        <a href="#home" class="logo">
+            Allandt Bik-Elliott
+        </a>
+
+        <a href=""></a>
+        <nav class="mobile-hidden">
+            <a href="#home">Home</a>
+            <a href="#work">Work</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+        </nav>
+    </div>
+</template>
+
+<style lang="scss">
+.main-nav {
+    display: flex;
+    justify-content: space-between;
+    padding: 1em;
+    position: fixed;
+    width: 100%;
+    background-color: var(--matte);
+    font-weight: 600;
+
+    .logo {
+        padding: 1em;
+    }
+
+    nav {
+        a {
+            display: inline-block;
+            padding: 1em;
+        }
+    }
+}
+</style>
