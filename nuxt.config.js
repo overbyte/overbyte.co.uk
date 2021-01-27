d9fd7415dda1e3ac978a3761d3e64ae6d04dcb3e
Allandt Bik-Elliott <allandt@gmail.com>
2021-01-27 23:52:59 +0000

    add axios, dotenv and set up SB_TOKEN and SB_CV environment variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,45 +1,53 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+    // Target: https://go.nuxtjs.dev/config-target
+    target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-      title: 'Allandt Bik-Elliott :: Developer',
-    htmlAttrs: {
-      lang: 'en'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'Allandt Bik-Elliott :: Developer',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+        'normalize.css/normalize.css',
+        '~assets/css/main.scss'
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-      'normalize.css/normalize.css',
-      '~assets/css/main.scss'
-  ],
+    // environment variables
+    env: {
+        SB_TOKEN: process.env.SB_TOKEN,
+    },
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+    ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+    ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+    ],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+    }
 }
